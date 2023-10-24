01ea46424ae79b4430fb9c4c9e86b93fc3058527
souissisamar <131592677+souissisamar@users.noreply.github.com>
2023-10-24 15:00:49 +0200

    GestionsClients

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+cout<<"Gestion Vlients"<<endl;
+QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+
+
+
+
+
+ return a.exec();
+
+}
